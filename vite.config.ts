fb975ddf704214c4e7f99c60b0a7650da75af46f
Mwadali <wellingtonmwadali@gmail.com>
2024-03-01 09:40:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+    // Specify the output directory for the build artifacts
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
